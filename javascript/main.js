3190019b63c1b917eb3d2e6d968087deb352ba92
redmine <estou.eu.te.amo@gmail.com>
2021-05-08 21:11:50 +0900

    abc200_a

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,3 @@
-const input = require('fs').readFileSync('/dev/stdin', 'UTF-8').split('');
+const input = require('fs').readFileSync('/dev/stdin', 'UTF-8') * 1;
 
-let line = '';
-let isReverse = false;
-
-input.forEach((char) => {
-	if (char === 'R') {
-		isReverse = !isReverse;
-	} else if (isReverse) {
-		line = char + line;
-	} else {
-		line = line + char;
-	}
-});
-
-const array = line.split('');
-
-if (isReverse) {
-	array.reverse();
-}
-const T = new Array();
-for (const char of array) {
-	if (T.length && T[T.length - 1] === char) {
-		T.pop();
-	} else {
-		T.push(char);
-	}
-}
-
-console.log(T.join(''));
+console.log(Math.floor((input - 1) / 100 + 1));

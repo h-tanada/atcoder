32d0ff9a9388d03bc93035a49d7000e24d8efebc
redmine <estou.eu.te.amo@gmail.com>
2021-05-08 22:04:38 +0900

    abc200_c

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,35 @@
-const input = require('fs').readFileSync('/dev/stdin', 'UTF-8').split(' ');
+const input = require('fs').readFileSync('/dev/stdin', 'UTF-8').split('\n');
 
-let [N, K] = input;
+const N = input.shift();
 
-for (let count = 0; count < K * 1; count++) {
-	N = N * 1;
-	if (N % 200 == 0) {
-		N = N / 200;
-	} else {
-		N = N + '200';
+const target = new Map();
+
+const As = input.shift().split(' ');
+
+As.length = N * 1;
+
+As.map((A) => (A * 1) % 200).forEach((num) => {
+	if (!target.has(num)) {
+		target.set(num, 0);
 	}
+	target.set(num, target.get(num) + 1);
+});
+
+const keys = Array.from(target.keys());
+
+let result = Array.from(target.values())
+	.map((value) => (value * (value - 1)) / 2)
+	.reduce((a, b) => a + b, 0);
+
+while (keys.length > 1) {
+	const a = keys.pop();
+	const aCount = target.get(a);
+	keys.forEach((b) => {
+		const bCount = target.get(b);
+		if ((a - b) % 200 == 0) {
+			result += aCount * bCount;
+		}
+	});
 }
 
-console.log(N);
+console.log(result);

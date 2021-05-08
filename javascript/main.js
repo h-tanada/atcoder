1ddb50c735bfed559d87025d2a46c365bd7c215d
redmine <estou.eu.te.amo@gmail.com>
2021-05-08 09:52:57 +0900

    zone2021 C

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,73 @@
 const input = require('fs').readFileSync('/dev/stdin', 'UTF-8').split('\n');
 
-const [N, D, H] = input.shift().split(' ');
-const calc = (ufoDistance, ufoHeight) => (distance, height) => {
-	const slope = (ufoHeight - height) / (ufoDistance - distance);
-	return ufoHeight - slope * ufoDistance;
-};
+const N = input.shift().split(' ');
 input.length = N * 1;
-const calcurator = calc(D * 1, H * 1);
-const maxHeight = input
-	.map((line) => {
-		const distance = line.split(' ')[0] * 1;
-		const height = line.split(' ')[1] * 1;
-		return calcurator(distance, height);
-	})
-	.reduce((a, b) => Math.max(a, b));
-console.log(maxHeight > 0 ? maxHeight : 0);
+
+class Member {
+	constructor(number, [a, b, c, d, e]) {
+		this.number = number;
+		this.param = { a, b, c, d, e };
+	}
+	set mid(mid) {
+		this.bit =
+			((this.a >= mid) << 0) +
+			((this.b >= mid) << 1) +
+			((this.c >= mid) << 2) +
+			((this.d >= mid) << 3) +
+			((this.e >= mid) << 4);
+	}
+	get a() {
+		return this.param.a || 0;
+	}
+	get b() {
+		return this.param.b || 0;
+	}
+	get c() {
+		return this.param.c || 0;
+	}
+	get d() {
+		return this.param.d || 0;
+	}
+	get e() {
+		return this.param.e || 0;
+	}
+}
+
+const members = input.map(
+	(line, index) =>
+		new Member(
+			index,
+			line.split(' ').map((num) => num * 1)
+		)
+);
+
+const check2 = (mid, members = new Array()) => {
+	const calcMembers = members
+		.map((member) => {
+			member.mid = mid;
+			return member;
+		})
+		.filter((member, index, self) => self.findIndex((elm) => elm.bit === member.bit) == index);
+	return calcMembers.some((member1) =>
+		calcMembers.some((member2) =>
+			calcMembers.some((member3) => {
+				console.table({ member1, member2, member3 });
+				return (member1.bit | member2.bit | member3.bit) === 31;
+			})
+		)
+	);
+};
+
+let min = 0,
+	max = 1e9 + 1;
+
+while (Math.abs(min - max) > 1) {
+	const mid = Math.floor((min + max) / 2);
+	const res = check2(mid, members);
+	if (res) {
+		min = mid;
+	} else {
+		max = mid;
+	}
+}
+console.log(min);
